e0ed834a893b34bb90366755a1a2a8f324c670ca
caleward <caleward9@gmail.com>
2021-08-16 08:49:09 -0400

    movie-search using materialize CSS

---
+++ src/components/Filter.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./filter.css";
+
+function Filter() {
+
+    const [isOpen, setIsOpen] = useState(false);
+  
+    return (
+      <div classname="filter">
+
+      </div>
+      
+    );
+  }
