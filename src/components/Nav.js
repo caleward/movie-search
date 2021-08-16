0c0fb402636e21edc1205162ac60c06acbf12bc4
caleward <caleward9@gmail.com>
2021-08-16 09:20:54 -0400

    movie-search

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav className="teal">
+            <div className="nav-wrapper container">
+                <a href="#" onClick={()} className="brand-logo">A Movie Finder  </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
